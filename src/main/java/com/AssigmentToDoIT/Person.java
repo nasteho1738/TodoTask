8598eb6aa1bce4ef075b23b115de18a9e9fbf859
Nasteho Cali <nasteho1738@icloud.com>
2024-03-20 15:03:20 +0100

    done

---
+++ src/main/java/com/AssigmentToDoIT/Person.java
@@ -0,0 +1,10 @@
+package com.AssigmentToDoIT;
+
+Import java.time.Local.Date;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/AssigmentToDoIT/ToDoItem.java
@@ -0,0 +1,2 @@
+package com.AssigmentToDoIT;public class ToDoItem {
+}
